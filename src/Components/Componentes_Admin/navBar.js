5eaef05417fa054a05240a3ce5966880d4ba24f5
backup96 <109244276+backup96@users.noreply.github.com>
2024-11-16 07:41:58 -0500

    Más correcciones

--- src/Components/Componentes_Admin/navBar.js
+++ src/Components/Componentes_Admin/navBar.js
@@ -44,7 +44,7 @@ export function NavBar() {
       console.log(cookie, "Hola")
   };
 
-  useEffect(() => {getCookie(); }, []);
+  useEffect(() => {getCookie(); }, [name]);
 
   const handleDelete = () => {
     axios
