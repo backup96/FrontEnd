876d4a754191e47fa0a60496f71383c68e897dec
backup96 <109244276+backup96@users.noreply.github.com>
2024-11-17 23:30:06 -0500

    ASi

--- src/Components/Componentes_Propietario/tabla.js
+++ src/Components/Componentes_Propietario/tabla.js
@@ -267,7 +267,7 @@ ${proxy}/espacio_parqueadero?tipoEspacio=Carro`)
                 </div>
               </form>
               <h2 className="text-center">Moto</h2>
-              {console.log(hasRentedMoto, hasRentedCarro)}
+              {console.log(rentedSpaces)}
               {hasRentedMoto || hasRentedCarro ? (
                 <p className="text-center text-danger">
                   Ya has rentado un espacio. Excediste el limite de renta.
