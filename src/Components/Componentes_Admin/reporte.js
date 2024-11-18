7d0098ebdde46c22ffca46452e08147f8b82701c
backup96 <109244276+backup96@users.noreply.github.com>
2024-11-18 02:15:38 -0500

    Ultimos cambios

--- src/Components/Componentes_Admin/reporte.js
+++ src/Components/Componentes_Admin/reporte.js
@@ -126,7 +126,7 @@ const Reporte = ({ item, currentRecords, apiS }) => {
             <tr>
               <th colSpan="2">Numero de parqeuaderos rentados</th>
               <th rowSpan="1" colSpan="1">
-                {data[0].total}
+                {data}
               </th>
             </tr>
           </tfoot>
